7b2ad12c1320774a28b91ee1e820d63abe4700ca
Isaac Laster <isaac.laster812@gmail.com>
2025-04-30 18:50:23 -0700

    mage class complete

---
+++ python/classes/mage.py
@@ -0,0 +1,23 @@
+from player import Player
+
+class Mage(Player):
+    def __init__(self):
+        super().__init__(
+            strength=3,
+            mana=15,
+            endurance=4,
+            intelligence=10,
+            agility=6,
+            dexterity=5,
+            luck=5,
+            charisma=6,
+            wisdom=9
+        )
+
+    def cast_spell(self, spell_name, cost):
+        if self.mana >= cost:
+            self.mana -= cost
+            print(f"You cast {spell_name}! Remaining mana: {self.mana}")
+        else:
+            print(f"You don't have enough mana to cast {spell_name}.")
+
